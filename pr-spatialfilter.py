2e0216515eb82121be713886fb29170d1a2c404d
Ezequias Junior <ezequiasjunio@gmail.com>
2019-05-27 11:03:09 -0300

    item 4   - pratica 4

--- pr-spatialfilter.py
+++ pr-spatialfilter.py
@@ -1,78 +1,9 @@
-#%% [markdown]
-# ### Prática - Filtragem Espacial
-# 
-# cuidado na matriz A ela é N elementos x M ondas/fontes, eu pensei em M fontes por N elementos
-# 
-# A não varia no tempo considerando uma coerencia... no intervalo de tempo T a informação angular é a mesma
-# 
-# R = R sinal + R noise => EVD.... Us = subspaço do sinal | Un subspaço do ruído -> ortogonais
-# 
-# - o que é a matriz covariância
-# 
-# Us.H @ Un = 0
-# 
-# R amostral =  Maxima verossimilhança média de x@xH 
-# 
-# vetor de apontamento média do a($\theta$)
-# 
-# #### métodos de busca
-# 
-# correlação aH(x) @ a(x0) max w/ x = x0
-# 
-# cor(x) = aH(x) @ x(t) ... varrer com um x de um em um grau muda o gráfico...mais fino e variar o numero de antenas?
-# X é uma unica amostra.... será que varia de t ???
-# 
-# M maiores picos representam as estimativas,  quantos maiores que um treshhold 
-# acima de 45%
-# 
-# para o metodo do BF Convencional
-# 
-# formula do wBF slides formula que varia com theta
-# matriz R dependendo do ""buffer que armazena T"
-# X considera todas as amostras de T pq ele olha pra matriz R desempenho melhor
-# 
-# 
-# Para o Music utiliza a SVD de R na fórmula da potencia em funlçao de theta 
-#(subspaço do ruido)
-# picos do espectro explorando o conceito da ortogonalidade do sinal e do ruído 
-# 
-# achar uv.... N primeiros valores estão no sub do sinal, os M-N  
-# N primeiras colunas é sinal o resto é ruido \[coluna| ... | coluna\]
-# 
-# saber o numero de maiores autovalores .... e/emax dá o numero de angulos "" 
-#por cima"" ordenados....
-# 
-# ESPRIT solução fechada  
-# entra pseudo inversa us\[0:size-1\] e us\[1:size\]  
-# usa argumento numero complexo ( ver função pois retorna radiano)  
-# 
-# angulos diferentes Us rank completo
-# 
-# ---------------
-# ---------------
-# Erro entre os métodos o angulo que ele cai 
-# 
-# se for percorrer a estimativa a cada snr precisa fazer um monte carlo média 
-# dos valores
-# 
-# para os plots lembrar de normalizar pra bater certinho
-# 
-# Pratica vai variar o N, snapshot = T
-# pegar os 3 maiores picos , calcula o max, elimina, calcula o max, elimina, 
-#calcula o max = 3 máx
-# 
-# fazer um pandas pra complexidade ????
-# 
-# 10 db pra varia r o T... será que dá pra fazer uma superficie??
-# 
-# plot pelo numero de sensores.... 5 a 50
-# 
-# ver como fica o arranjo... e após a estimativa
-# 
-# 
 # para a uRA.... lembrar do vec... percorrer por matrizes se der tempo....
 # 
 # pra comparativo colocar só os pandas e falar...
+
+# calcular o tempo necessário de busca ? seria o time it pra comprarar
+# comparação justa todos os metods no mesmo realiz
 
 #%%
 #!/usr/bin/env python3
@@ -81,10 +12,7 @@ from numpy.random import rand, randn
 import matplotlib.pylab as plt
 from numpy.linalg import eig, pinv
 from scipy.signal import find_peaks
-
-# calcular o tempo necessário de busca ? seria o time it pra comprarar
-# comparação justa todos os metods no mesmo realiz
-
+import time
 #%%
 # Funções:
 def ula(N, d, theta):
@@ -92,10 +20,15 @@ def ula(N, d, theta):
     # Resposta do arranjo linear:
     return np.exp(-1j*2*np.pi*dx*np.sin(theta))
 
-def get_angles(x, angles):
+def get_angles(x, angles, theta):
     # Extração dos picos dos espectros espaciais:
-    idx = find_peaks(x.reshape(x.size,), height=0.45)
-    return np.rad2deg(angles[idx[0]])
+    idx = find_peaks(x.reshape(x.size,), height=0.25)
+    candidates = np.rad2deg(angles[idx[0]])
+    ang = np.zeros(M)
+    for idx, val in enumerate(theta):
+        aux = abs(candidates-val)
+        ang[idx] = candidates[(np.nonzero(aux==min(aux))[0][0])]
+    return ang 
 
 # Método da Correlação:
 def corr_bf(X, angles):
@@ -167,7 +100,6 @@ def esprt_bf(X, m, d):
     # Retornando ângulos estimados:
     return np.rad2deg(np.sort(np.arcsin(angles/(2*np.pi*d))))
 
-
 #%%
 # Parâmetros:
 snapshot = 500              # Snapshots
@@ -187,7 +119,6 @@ for idx, val in enumerate(theta_r):
 
 # Modelo de sinal:
 mt_X = np.zeros((snapshot, N, 1), dtype=complex)
-mt_R = np.zeros((N, N), dtype=complex)
 # Para cada snapshot:
 for t in range(snapshot):
     # Sinal de cada fonte:
@@ -225,7 +156,8 @@ a = np.rad2deg(angles)
 with plt.style.context('ggplot', True):
     plt.figure()
     plt.title('Comparativo - Métodos de Estimação de AoA')
-    
+    plt.xlabel('Ângulos [deg]')
+    plt.ylabel('Espectro Espacial Normalizado')
     plt.plot(a, corr, '-.', label='BF Corr.')
     plt.plot(a, beamf, '--', label='BF Conv.')
     plt.plot(a, music, '-', label='MUSIC')
@@ -249,15 +181,202 @@ with plt.style.context('ggplot', True):
     plt.show()
 
 #%%
-# ii=find_peaks((abs(beamf)/max(abs(beamf))).reshape(angles.size,), height=0.45)
-# jj=find_peaks((abs(music)/max(abs(music))).reshape(angles.size,), height=0.45)
-# kk=find_peaks((abs(corr)/max(abs(corr))).reshape(angles.size,), height=0.45)
+# RMSE vs SNR:
+def sim_rmse(snr, mt_A, theta, angles):
+    # Parâmetros:
+    snapshot = 500              # Snapshots
+    M = 3                       # Número de fontes de sinal
+    N = 10                      # Número de sensores do arranjo
+    d = 0.5                     # Espaçamento (lambda = 1)        
+    realiz = 100
+    rmse = np.zeros((4, snr.size))
+    for s in range(snr.size):
+        for i in range(realiz):
+            print(f'Realização: {i+1}')
+            snr_lin = 10**(0.1*snr[s])     # Relação sinal-ruído [lin]
+            # Modelo de sinal:
+            mt_X = np.zeros((snapshot, N, 1), dtype=complex)
+            # Para cada snapshot:
+            for t in range(snapshot):
+                # Sinal de cada fonte:
+                signal = rand(M, 1) + 1j*rand(M, 1)
+                # Ruído de cada sensor:
+                vt_n = (randn(N, 1) + 1j*randn(N, 1))/np.sqrt(snr_lin)
+                # Sinal recebido no instante t:
+                mt_X[t] = mt_A@signal + vt_n
+            # Correlação:
+            # dt = 5
+            # c = np.zeros((dt, angles.size, 1))
+            # for i in range(dt):
+            #     c[i] = corr_bf(mt_X[i], angles)
+            # corr = np.mean(c, axis=0)
+            corr = corr_bf(mt_X[0], angles)
+            # Tratamento dos angulos:
+            ang_corr = get_angles(corr, angles, theta)
+            print('corr',ang_corr)
+            # BF convencional:
+            beamf = conv_bf(mt_X, angles)
+            ang_beamf = get_angles(beamf, angles, theta)
+            print('beam',ang_beamf)            
+            # MUSIC:
+            music = music_bf(mt_X, angles, M)
+            ang_music = get_angles(music, angles, theta)
+            print('mus',ang_music)            
+            # ESPRIT:
+            esprit = esprt_bf(mt_X, M, d)
+            print('sprt',esprit) 
+            # Cálculo do RMSE
+            rmse[0, s] += np.sqrt(np.mean((theta-ang_corr)**2))
+            rmse[1, s] += np.sqrt(np.mean((theta-ang_beamf)**2))
+            rmse[2, s] += np.sqrt(np.mean((theta-ang_music)**2))
+            rmse[3, s] += np.sqrt(np.mean((theta-esprit)**2))
+    # RMSE para cada snr:
+    return rmse/realiz
 
-# print(f'''
-# {3}
-# {np.rad2deg(angles[kk[0]])}
-# {np.rad2deg(angles[ii[0]])}
-# {np.rad2deg(angles[jj[0]])}
-# {np.rad2deg(esprit)}
-# ''')
-# np.arange(5).max()
+snr = np.arange(0, 31, 10)
+start = time.time()
+erros = sim_rmse(snr, mt_A, theta, angles)
+stop = time.time()
+print('done', stop-start)
+#%%
+with plt.style.context('ggplot', True):
+    plt.figure()
+    plt.title('RMSE vs SNR - Métodos de Estimação de AoA')
+    plt.xlabel('SNR [dB]')
+    plt.ylabel('RMSE')
+    plt.semilogy(snr, erros[0, :], label='Corr.')
+    plt.semilogy(snr, erros[1, :], label='BF Conv.')
+    plt.semilogy(snr, erros[2, :], label='MUSIC')
+    plt.semilogy(snr, erros[3, :], label='ESPRIT')
+    plt.legend(loc='best')
+    plt.savefig('per-snr100')
+    plt.show()
+
+#%%
+# RMSE vs Snapshots:
+def sim_rmse_snap(snapshots, mt_A, theta, angles):
+    # Parâmetros:
+    snr = 10                    # Relação sinal-ruído [dB]  
+    M = 3                       # Número de fontes de sinal
+    N = 10                      # Número de sensores do arranjo
+    d = 0.5                     # Espaçamento (lambda = 1)        
+    realiz = 100
+    rmse = np.zeros((4, snapshots.size))
+    for s in range(snapshots.size):
+        for i in range(realiz):
+            print(f'Realização: {i+1} | Snapsots: {snapshots[s]}')
+            snr_lin = 10**(0.1*snr)     # Relação sinal-ruído [lin]
+            # Modelo de sinal:
+            mt_X = np.zeros((snapshots[s], N, 1), dtype=complex)
+            # Para cada snapshot:
+            for t in range(snapshots[s]):
+                # Sinal de cada fonte:
+                signal = rand(M, 1) + 1j*rand(M, 1)
+                # Ruído de cada sensor:
+                vt_n = (randn(N, 1) + 1j*randn(N, 1))/np.sqrt(snr_lin)
+                # Sinal recebido no instante t:
+                mt_X[t] = mt_A@signal + vt_n
+            # Correlação:
+            corr = corr_bf(mt_X[0], angles)
+            # Tratamento dos angulos:
+            ang_corr = get_angles(corr, angles, theta)
+            print('corr',ang_corr)
+            # BF convencional:
+            beamf = conv_bf(mt_X, angles)
+            ang_beamf = get_angles(beamf, angles, theta)
+            print('beam',ang_beamf)            
+            # MUSIC:
+            music = music_bf(mt_X, angles, M)
+            ang_music = get_angles(music, angles, theta)
+            print('mus',ang_music)            
+            # ESPRIT:
+            esprit = esprt_bf(mt_X, M, d)
+            print('sprt',esprit) 
+            # Cálculo do RMSE
+            rmse[0, s] += np.sqrt(np.mean((theta-ang_corr)**2))
+            rmse[1, s] += np.sqrt(np.mean((theta-ang_beamf)**2))
+            rmse[2, s] += np.sqrt(np.mean((theta-ang_music)**2))
+            rmse[3, s] += np.sqrt(np.mean((theta-esprit)**2))
+    # RMSE para cada snapshot:
+    return rmse/realiz
+
+snapshots = np.array([10, 20, 40, 80, 150, 300, 600, 1000])
+start = time.time()
+erroSnap = sim_rmse_snap(snapshots, mt_A, theta, angles)
+stop = time.time()
+print('done', stop-start)
+
+#%%
+with plt.style.context('ggplot', True):
+    plt.figure()
+    plt.title('RMSE vs Snapshots - Métodos de Estimação de AoA')
+    plt.xlabel('SNR [dB]')
+    plt.ylabel('RMSE')
+    plt.semilogy(snapshots, erroSnap[0, :], label='Corr.')
+    plt.semilogy(snapshots, erroSnap[1, :], label='BF Conv.')
+    plt.semilogy(snapshots, erroSnap[2, :], label='MUSIC')
+    plt.semilogy(snapshots, erroSnap[3, :], label='ESPRIT')
+    plt.legend(loc='best')
+    plt.savefig('per-snap100')
+    plt.show()
+
+
+#%%
+# RMSE vs N:
+def sim_rmse_n(Num, theta, angles):
+    # Parâmetros:
+    snapshot = 500              # Snapshots
+    snr = 10                    # Relação sinal-ruído [dB]  
+    M = 3                       # Número de fontes de sinal
+    d = 0.5                     # Espaçamento (lambda = 1)        
+    realiz = 1
+    rmse = np.zeros((4, Num.size))
+    for n in range(Num.size):
+        N=Num[n]
+        for i in range(realiz):
+            print(f'Realização: {i+1} | N: {N}')
+            snr_lin = 10**(0.1*snr)     # Relação sinal-ruído [lin]
+            # Modelo de sinal:
+            mt_X = np.zeros((snapshot, N, 1), dtype=complex)
+            # Para cada snapshot:
+            print(N)
+            for t in range(snapshot):
+                # Sinal de cada fonte:
+                signal = rand(M, 1) + 1j*rand(M, 1)
+                # Ruído de cada sensor:
+                vt_n = (randn(N, 1) + 1j*randn(N, 1))/np.sqrt(snr_lin)
+                # Construção da matriz de resposta do arranjo:
+                mt_A = np.zeros((N,M), dtype=complex)
+                for idx, val in enumerate(theta_r):
+                    mt_A[:, idx] = ula(N, d, val)
+                # Sinal recebido no instante t:
+                mt_X[t] = mt_A@signal + vt_n
+            # Correlação:
+            corr = corr_bf(mt_X[0], angles)
+            # Tratamento dos angulos:
+            ang_corr = get_angles(corr, angles, theta)
+            print('corr',ang_corr)
+            # BF convencional:
+            beamf = conv_bf(mt_X, angles)
+            ang_beamf = get_angles(beamf, angles, theta)
+            print('beam',ang_beamf)            
+            # MUSIC:
+            music = music_bf(mt_X, angles, M)
+            ang_music = get_angles(music, angles, theta)
+            print('mus',ang_music)            
+            # ESPRIT:
+            esprit = esprt_bf(mt_X, M, d)
+            print('sprt',esprit) 
+            # Cálculo do RMSE
+            rmse[0, s] += np.sqrt(np.mean((theta-ang_corr)**2))
+            rmse[1, s] += np.sqrt(np.mean((theta-ang_beamf)**2))
+            rmse[2, s] += np.sqrt(np.mean((theta-ang_music)**2))
+            rmse[3, s] += np.sqrt(np.mean((theta-esprit)**2))
+    # RMSE para cada snapshot:
+    return rmse/realiz
+
+N = np.arange(5, 51, 5)
+start = time.time()
+erroN = sim_rmse_n(N, theta, angles)
+stop = time.time()
+print('done', stop-start)
