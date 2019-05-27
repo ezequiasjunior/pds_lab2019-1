984ee70bb3525bbff15292c0618d36e3ebcccd77
Ezequias Junior <ezequiasjunio@gmail.com>
2019-05-27 02:49:06 -0300

    gradico item 1 - pratica 4

--- pr-spatialfilter.py
+++ pr-spatialfilter.py
@@ -57,7 +57,7 @@
 # 
 # para os plots lembrar de normalizar pra bater certinho
 # 
-# Pratica não vai variar o N, snapshot = T
+# Pratica vai variar o N, snapshot = T
 # pegar os 3 maiores picos , calcula o max, elimina, calcula o max, elimina, 
 #calcula o max = 3 máx
 # 
@@ -79,28 +79,94 @@
 import numpy as np 
 from numpy.random import rand, randn
 import matplotlib.pylab as plt
-from numpy.linalg import eig
-# correlação tbm....
+from numpy.linalg import eig, pinv
+from scipy.signal import find_peaks
 
-'''
-snr[lin] =  10^(0.1*snr)
-para a snr tem o ruído do sinal como inverso... snr =1/sigma^2 , simga = 1/sqrt
-#(snr[lin])
+# calcular o tempo necessário de busca ? seria o time it pra comprarar
+# comparação justa todos os metods no mesmo realiz
 
-fazer funções para criação de arranjos -> parametro (n, theta)
-
-plots de corr x angulos, fazer uma reta para eixo x = angulo desejado
-meu angulos tao em radiano no plot mudar os valores para deg
-
-plots de pot do BF pelo angulo
-
-calcular o tempo necessário de busca ? seria o time it pra comprarar
-'''
 #%%
+# Funções:
 def ula(N, d, theta):
     dx = np.arange(N)*d  # Indices dos espaçamentos "(nx-1)*dx"
     # Resposta do arranjo linear:
     return np.exp(-1j*2*np.pi*dx*np.sin(theta))
+
+def get_angles(x, angles):
+    # Extração dos picos dos espectros espaciais:
+    idx = find_peaks(x.reshape(x.size,), height=0.45)
+    return np.rad2deg(angles[idx[0]])
+
+# Método da Correlação:
+def corr_bf(X, angles):
+    corr = np.zeros((angles.size, 1), dtype=complex)
+    for idx, a in enumerate(angles):
+        corr[idx] = np.conj(ula(N, d, a)).T@X
+    # Normalização:
+    corr = np.abs(corr)
+    return corr/(corr.max())
+
+# Método Beamformer Convencional:
+def conv_bf(X, angles):
+    # Matriz de covariância estimada:
+    mt_R = np.zeros((X.shape[1], X.shape[1]), dtype=complex)
+    for t in range(X.shape[0]):
+        mt_R += (X[t]@np.conj(X[t]).T)
+
+    # Cálculo da matriz de covariância estimada:
+    mt_R /= X.shape[0]
+    # Espectro espacial:
+    Pbf = np.zeros((angles.size, 1), dtype=complex)
+    for idx, a in enumerate(angles):
+        st_vec = ula(N, d, a)
+        Pbf[idx] = np.conj(st_vec).T@mt_R@st_vec/\
+                   np.sqrt(np.conj(st_vec).T@st_vec)
+    # Normalização:
+    Pbf = np.abs(Pbf)
+    return Pbf/(Pbf.max())
+
+# Método MUSIC
+def music_bf(X, angles, m):
+    # Matriz de covariância estimada:
+    mt_R = np.zeros((X.shape[1], X.shape[1]), dtype=complex)
+    for t in range(X.shape[0]):
+        mt_R += (X[t]@np.conj(X[t]).T)
+
+    # Cálculo da matriz de covariância estimada:
+    mt_R /= X.shape[0]
+    # Decomposição em autovalores de R:
+    evalue, evec = eig(mt_R)
+    Uv = evec[:, m:]    # Subespaço do ruído
+    # Espectro espacial:
+    Pmsc = np.zeros((angles.size, 1), dtype=complex)
+    for idx, a in enumerate(angles):
+        st_vec = ula(N, d, a)
+        Pmsc[idx] = np.conj(st_vec).T@st_vec/\
+                np.sqrt(np.conj(st_vec).T@Uv@np.conj(Uv).T@st_vec)
+    # Normalização:    
+    Pmsc = np.abs(Pmsc)
+    return Pmsc/(Pmsc.max())
+
+# Método ESPRIT:
+def esprt_bf(X, m, d):
+    # Matriz de covariância estimada:
+    mt_R = np.zeros((X.shape[1], X.shape[1]), dtype=complex)
+    for t in range(X.shape[0]):
+        mt_R += (X[t]@np.conj(X[t]).T)
+
+    # Cálculo da matriz de covariância estimada:
+    mt_R /= X.shape[0]
+    # Decomposição em autovalores de R:
+    evalue, evec = eig(mt_R)
+    # Estimação:
+    Us = evec[:, :-(X.shape[1]-m)]    # Subespaço do sinal
+    # mt_psi = pinv(Us[:-1, :]) @ Us[1:, :]
+    mt_psi = pinv(Us[1:, :]) @ Us[:-1, :]
+    val, vec = eig(mt_psi)
+    angles = np.angle(val)
+    # Retornando ângulos estimados:
+    return np.rad2deg(np.sort(np.arcsin(angles/(2*np.pi*d))))
+
 
 #%%
 # Parâmetros:
@@ -131,27 +197,11 @@ for t in range(snapshot):
     # Sinal recebido no instante t:
     mt_X[t] = mt_A@signal + vt_n
 
-# #------------------------------------------------------------------------------
-#     # Matriz de covariância estimada:
-#     mt_R += (mt_X[t]@np.conj(mt_X[t]).T)
-
-# # Cálculo da matriz de covariância estimada:
-# mt_R /= snapshot
-#-----------------------------------------------------------------------------
-# # Decomposição em autovalores de R:
-# evalue, evec = eig(mt_R)
-
 #%%
 # Método da Correlação:
-def corr_bf(X, angles):
-    corr = np.zeros((angles.size, 1), dtype=complex)
-    for idx, a in enumerate(angles):
-        corr[idx] = np.conj(ula(N, d, a)).T@X
-    return corr
-
 # Cálculo da média dos dt primeiros snapshots:
 dt = 5
-c = np.zeros((dt, angles.size, 1), dtype=complex)
+c = np.zeros((dt, angles.size, 1))
 for i in range(dt):
     c[i] = corr_bf(mt_X[i], angles)
 
@@ -159,65 +209,55 @@ corr = np.mean(c, axis=0)
 
 #%%
 # Método Beamformer Convencional:
-def conv_bf(X, angles):
-    # Matriz de covariância estimada:
-    mt_R = np.zeros((X.shape[1], X.shape[1]), dtype=complex)
-    for t in range(X.shape[0]):
-        mt_R += (X[t]@np.conj(X[t]).T)
-
-    # Cálculo da matriz de covariância estimada:
-    mt_R /= X.shape[0]
-    # Espectro espacial:
-    Pbf = np.zeros((angles.size, 1), dtype=complex)
-    for idx, a in enumerate(angles):
-        Pbf[idx] = np.conj(ula(N, d, a)).T@mt_R@ula(N, d, a)/\
-                   np.sqrt(np.conj(ula(N, d, a)).T@ula(N, d, a))
-    
-    return Pbf
-
 beamf = conv_bf(mt_X, angles)
 
 #%%
 # Método MUSIC
-def music_bf(X, angles):
-    # Matriz de covariância estimada:
-    mt_R = np.zeros((X.shape[1], X.shape[1]), dtype=complex)
-    for t in range(X.shape[0]):
-        mt_R += (X[t]@np.conj(X[t]).T)
+music = music_bf(mt_X, angles, M)
 
-    # Cálculo da matriz de covariância estimada:
-    mt_R /= X.shape[0]
-    # Decomposição em autovalores de R:
-    evalue, evec = eig(mt_R)
+#%%
+# Método ESPRIT:
+esprit = esprt_bf(mt_X, M, d)
+
+
+#%%
+a = np.rad2deg(angles)
+with plt.style.context('ggplot', True):
+    plt.figure()
+    plt.title('Comparativo - Métodos de Estimação de AoA')
     
+    plt.plot(a, corr, '-.', label='BF Corr.')
+    plt.plot(a, beamf, '--', label='BF Conv.')
+    plt.plot(a, music, '-', label='MUSIC')
 
-    return
+    plt.plot(esprit, 0.75*(np.ones(M)), linestyle='none', marker='o', 
+                                        color='r', 
+                                        markersize=3, fillstyle='full', 
+                                        label='ESPRIT')
+
+    plt.plot(theta, 0.75*(np.ones(M)), linestyle='none', color='b', 
+                                        marker='o', 
+                                        markersize=8, fillstyle='none', 
+                                        label='$\Theta$ Exato')
+    # plt.grid()
+    for value in theta:
+        plt.plot(value*np.ones(100), np.linspace(0,1,100), 'k:', 
+                 label=f'$\Theta$ = {value}°')
+    # plt.xticks(np.arange(0, 91, 5))
+    plt.axis([0, 90, 0, 1.01])
+    plt.legend(loc='best')
+    plt.show()
 
 #%%
-# mudar x ticks
-plt.figure()
-# plt.grid()
-plt.plot(np.rad2deg(angles), np.abs(corr)/np.max(abs(corr)))
-for value in theta:
-    plt.plot(value*np.ones(100),np.linspace(0,1,100),'--')
-plt.show()
-# FAZER FUNÇÃO PARA EXTRAIR O m MAIORES VALORES..
-# comparação justa todos os metods no mesmo realiz
+# ii=find_peaks((abs(beamf)/max(abs(beamf))).reshape(angles.size,), height=0.45)
+# jj=find_peaks((abs(music)/max(abs(music))).reshape(angles.size,), height=0.45)
+# kk=find_peaks((abs(corr)/max(abs(corr))).reshape(angles.size,), height=0.45)
 
-#%%
-# mudar x ticks
-plt.figure()
-# plt.grid()
-plt.plot(np.rad2deg(angles), np.abs(beamf)/np.max(abs(beamf)))
-for value in theta:
-    plt.plot(value*np.ones(100),np.linspace(0,1,100),'--')
-plt.show()
-#%%
-np.set_printoptions(2)
-print(f'''
-Cov:
-{mt_A}
-
-{evec.shape}
-{angles.shape}
-''')
+# print(f'''
+# {3}
+# {np.rad2deg(angles[kk[0]])}
+# {np.rad2deg(angles[ii[0]])}
+# {np.rad2deg(angles[jj[0]])}
+# {np.rad2deg(esprit)}
+# ''')
+# np.arange(5).max()
